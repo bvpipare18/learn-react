54e9628284319b95d83ca602eb73a3f508b6ae8b
Bharat Pipare <bharatpipare987@gmail.com>
2025-02-17 18:33:59 +0530

    04: explore about what is jsx; how to use jsx expression; use jsx expression in javascript function

--- src/index.js
+++ src/index.js
@@ -8,10 +8,42 @@ import reportWebVitals from './reportWebVitals';
 function MyButton() {
   return <button>Click me</button>;
 }
+
+// use embedded JSX expression inside of a component
+function MyComponent() {
+  const name = "Virat Kohli";
+  const age = 35;
+  const element = <h1>Hello, {name} {age}</h1>;
+  return element;
+}
+
+
+// use Javascript function in JSX expression
+function add(x, y){
+  return x+y;
+}
+function AddNumbers(){
+  return <h1>Let's add numbers {add(10,5)}</h1>
+}
+
+
+// dynamic attributes
+
+
+function RenderDynamicAttr() {
+  const imageURL = "https://images.unsplash.com/photo-1618886487325-f665032b6352?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+const linkToSite = "www.example.com"
+const imageElement = <img src={imageURL}></img>
+const linkElement = <a href={linkToSite}>click me</a>
+  return linkElement;
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <RenderDynamicAttr/>
+    <MyComponent/>
     <MyButton/>
+    <AddNumbers/>
     <App />
   </React.StrictMode>
 );
